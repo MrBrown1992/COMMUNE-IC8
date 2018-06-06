ac323b5a286de1ee93f6449e2c0d07f1da606c37
Nikolaus Spieß <nikolaus.spiess@edu.fh-joanneum.at>
2018-06-06 19:52:17 +0200

    added grocery class

---
+++ src/at/fh/swenga/model/Grocery.java
@@ -0,0 +1,76 @@
+package at.fh.swenga.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "grocery")
+public class Grocery {
+
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private String id;
+
+	@Column(name = "name", nullable = false, length = 60)
+	private String name;
+	@Column(name = "bought", nullable = false)
+	private boolean bought;
+	
+	public Grocery() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Grocery(String name, boolean bought) {
+		super();
+		this.name = name;
+		this.bought = bought;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the bought
+	 */
+	public boolean isBought() {
+		return bought;
+	}
+
+	/**
+	 * @param bought the bought to set
+	 */
+	public void setBought(boolean bought) {
+		this.bought = bought;
+	}
+
+	
+}
