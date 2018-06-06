6604a2977219bf645393975db3890ac089c84b25
Nikolaus Spieß <nikolaus.spiess@edu.fh-joanneum.at>
2018-06-06 20:55:33 +0200

    added Controllers and Daos

---
+++ src/at/fh/swenga/controller/GroceryController.java
@@ -0,0 +1,5 @@
+package at.fh.swenga.controller;
+
+public class GroceryController {
+
+}
---
+++ src/at/fh/swenga/dao/GroceryDao.java
@@ -0,0 +1,50 @@
+package at.fh.swenga.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import at.fh.swenga.model.Grocery;
+
+@Repository
+@Transactional
+public class GroceryDao {
+
+	@PersistenceContext
+	protected EntityManager entityManager;
+
+	public List<Grocery> getGroceries() {
+		TypedQuery<Grocery> typedQuery = entityManager.createQuery("select g from Grocery g", Grocery.class);
+		List<Grocery> typedResultList = typedQuery.getResultList();
+		return typedResultList;
+	}
+
+	public Grocery getGrocery(int i) throws DataAccessException {
+		return entityManager.find(Grocery.class, i);
+	}
+
+	public void persist(Grocery grocery) {
+		entityManager.persist(grocery);
+	}
+
+	public Grocery merge(Grocery grocery) {
+		return entityManager.merge(grocery);
+	}
+
+	public void delete(Grocery grocery) {
+		entityManager.remove(grocery);
+	}
+
+	public void delete(int id) {
+		Grocery grocery = getGrocery(id);
+		if (grocery != null)
+			delete(grocery);
+	}
+
+}
