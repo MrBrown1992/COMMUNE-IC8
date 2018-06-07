fbb8b80b096e0a1e276bc5c58436af1c0c20dbbd
MrPink1992 <maximilian.steiner@edu.fh-joanneum.at>
2018-06-07 11:43:10 +0200

    testing

--- src/at/fh/swenga/controller/SecurityController.java
+++ src/at/fh/swenga/controller/SecurityController.java
@@ -4,12 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-import at.fh.swenga.dao.UserRoleDao;
 import at.fh.swenga.dao.UserDao;
-
+import at.fh.swenga.dao.UserRoleDao;
 import at.fh.swenga.model.User;
 import at.fh.swenga.model.UserRole;
 
