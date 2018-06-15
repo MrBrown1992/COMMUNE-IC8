4639e474c4e1027c1d8002223b637d2eff119957
Nikolaus Spieß <nikolaus.spiess@edu.fh-joanneum.at>
2018-06-15 11:05:28 +0200

    added pdf dependencies in the pom.xml

---
+++ src/at/fh/swenga/dao/ImageDao.java
@@ -0,0 +1,15 @@
+package at.fh.swenga.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import at.fh.swenga.model.Image;
+
+@Repository
+@Transactional
+public interface ImageDao  extends JpaRepository<Image, Integer>{
+
+}
