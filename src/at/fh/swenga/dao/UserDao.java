5309d8600d33b1e7c93a5224e2db347808d68876
Nikolaus Spieß <nikolaus.spiess@edu.fh-joanneum.at>
2018-06-17 19:06:58 +0200

    minor fixes

--- src/at/fh/swenga/dao/UserDao.java
+++ src/at/fh/swenga/dao/UserDao.java
@@ -1,23 +1,14 @@
 package at.fh.swenga.dao;
 
-import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
-
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import at.fh.swenga.model.Grocery;
 import at.fh.swenga.model.User;
-
 
 @Repository
 @Transactional
-public interface UserDao extends JpaRepository<User, Integer>{
-	
-	
+public interface UserDao extends JpaRepository<User, Integer> {
+
 	public User findFirstByUsername(String username);
 }
