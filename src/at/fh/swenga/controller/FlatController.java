916cb8e7a9ae71ef5cfbf3b4ebc80aa727955360
MrPink1992 <maximilian.steiner@edu.fh-joanneum.at>
2018-06-18 15:41:10 +0200

    minor changes

--- src/at/fh/swenga/controller/FlatController.java
+++ src/at/fh/swenga/controller/FlatController.java
@@ -2,12 +2,16 @@ package at.fh.swenga.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import at.fh.swenga.dao.FlatDao;
 import at.fh.swenga.model.Flat;
@@ -18,22 +22,41 @@ public class FlatController {
 
 	@Autowired
 	FlatDao flatDao;
-	
+
 	public FlatController() {
 
 		// TODO Auto-generated constructor stub
 	}
-	
-	
-	
-	@RequestMapping(value = {"listFlat"})
+
+	@RequestMapping(value = { "listFlat" })
 	public String listFlat(Model model) {
-		
 
 		List<Flat> flats = flatDao.findAll();
 
-		model.addAttribute("flats",flats);
+		model.addAttribute("flats", flats);
 		return "listFlat";
+	}
+
+	@RequestMapping(value = "addFlat")
+	public String addFlat() {
+
+		return "editFlat";
+	}
+
+	@RequestMapping(value = { "createNewFlat" })
+	public String createNewFlat(Model model, @Valid Flat newFlat, @RequestParam(value = "flatName") String flatName,
+			Authentication authentication) {
+
+		newFlat.setName(flatName);
+		
+		flatDao.save(newFlat);
+		
+		return "forward:listFlat";
+	}
+
+	@RequestMapping(value = "editFlat")
+	public String editFlat() {
+		return "editFlat";
 	}
 	
 }
