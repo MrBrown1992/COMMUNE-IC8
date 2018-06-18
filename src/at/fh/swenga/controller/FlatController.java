3b4d7502ce712b2a703abc4e3aaf7451f99b0b5c
MrPink1992 <maximilian.steiner@edu.fh-joanneum.at>
2018-06-18 14:40:26 +0200

    added FlatDao, FlatControlle

---
+++ src/at/fh/swenga/controller/FlatController.java
@@ -0,0 +1,39 @@
+package at.fh.swenga.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import at.fh.swenga.dao.FlatDao;
+import at.fh.swenga.model.Flat;
+
+@Controller
+@Scope(proxyMode = ScopedProxyMode.TARGET_CLASS, value = "session")
+public class FlatController {
+
+	@Autowired
+	FlatDao flatDao;
+	
+	public FlatController() {
+
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+	@RequestMapping(value = {"listFlat"})
+	public String listFlat(Model model) {
+		
+
+		List<Flat> flats = flatDao.findAll();
+
+		model.addAttribute("flats",flats);
+		return "listFlat";
+	}
+	
+}
